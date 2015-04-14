37617fd83190714fa930247db762e8312df96fad
Guilherme <guilherme_otran@hotmail.com>
2015-04-14 19:58:57 -0300

    keyword partial

--- ReceiptForm.cs
+++ ReceiptForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Projeto_PI
 {
-    public class ReceiptForm : Form
+    public partial class ReceiptForm : Form
     {
         public ReceiptForm(string message)
         {
